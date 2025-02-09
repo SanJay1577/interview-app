99ddb29ebf7b2be800308f998e4f46d31f626621
SanJay1577 <sanjayprashanth1997@gmail.com>
2025-02-09 12:31:03 +0530

    complete backend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import { dbConnection } from "./db.js";
 import { userRouter } from "./Routes/user.js";
 import { notesRouter } from "./Routes/notes.js";
@@ -10,17 +10,17 @@ import { isAuthenticated } from "./controllers/auth.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 // middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 //db connection
-dbConnection()
+dbConnection();
 
 //routes
 
 app.use("/api/user", userRouter);
-app.use("/api/notes",isAuthenticated, notesRouter)
+app.use("/api/notes", isAuthenticated, notesRouter);
 
 //server connection
-app.listen(PORT, ()=>console.log(`Server running in localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server running in localhost:${PORT}`));
--- models/users.js
+++ models/users.js
@@ -1,33 +1,31 @@
 import mongoose from "mongoose";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema(
-    {
-        name : {
-            type : String,
-            required : true,
-            maxlength : 32,
-            trim : true
-        },
-        email : {
-            type:String,
-            required : true,
-            unique : true,
-            trim:true,
-        },
-        contact :{ 
-           type :Number,
-        },
-        password : {
-            type:String,
-            required : true
-        }
-    }
-)
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  contact: {
+    type: Number,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-const generateJwtToken = (id)=>{
-    return jwt.sign({id}, process.env.SECRET_KEY)
-}
+const generateJwtToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET_KEY);
+};
 
-const User = mongoose.model("user",userSchema);
-export {User, generateJwtToken}
+const User = mongoose.model("user", userSchema);
+export { User, generateJwtToken };
